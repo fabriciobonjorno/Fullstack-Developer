eb36cb4066ed9a2316875a421de0ed6df75a114b
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2023-03-07 01:12:35 -0500

    Corrected serializer for image avatar

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,3 +1,6 @@
 class Api::V1::UserSerializer < ActiveModel::Serializer
-  attributes :id, :full_name, :role, :url_image, :avatar_image
+  attributes :id, :full_name, :role, :url_image, :avatar
+  def avatar
+    object.avatar_image.attached? ? rails_blob_url(object.avatar_image) : false
+  end
 end
